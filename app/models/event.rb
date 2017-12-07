adb34a2c8d388d4e9feeb2370b76f34367d179e0
preeti-puja <preeti.puja18@gmail.com>
2017-12-07 13:52:42 +0530

    nested form for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :user
-  has_many   :tasks
+  has_many   :tasks, inverse_of: :event
   accepts_nested_attributes_for :tasks
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,3 @@
 class Task < ApplicationRecord
-  belongs_to :event
+  belongs_to :event, inverse_of: :tasks
 end
